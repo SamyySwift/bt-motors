a906f090383c86a8f782d950ad88f5e12f82a217
SamSwift <samueludofia94@gmail.com>
2026-05-02 22:13:37 +0100

    refactor: remove unused AnimatePresence import from Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X, Search } from "lucide-react";
 import { cn } from "../lib/utils";
